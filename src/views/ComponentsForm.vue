dc3072517f5c0d6c3a931035f645b49ea726b264
DanielMa <mwldaniel_008@126.com>
2022-04-10 07:04:17 +0800

    Submitting bigger forms

--- src/views/ComponentsForm.vue
+++ src/views/ComponentsForm.vue
@@ -1,41 +1,41 @@
 <template>
   <div>
     <h1>Create an Event</h1>
-    <form>
+    <form @submit="submit">
       <BaseSelect
         label="Select a category"
         :options="categories"
-        v-model=""
-        :error=""
+        v-model="category"
+        :error="errors.category"
       />
 
       <h3>Name & describe your event</h3>
       <BaseInput
         label="Title"
-        v-model=""
-        :error=""
+        v-model="title"
+        :error="errors.title"
         type="text"
       />
 
       <BaseInput
         label="Description"
-        v-model=""
-        :error=""
+        v-model="description"
+        :error="errors.description"
         type="text"
       />
 
       <h3>Where is your event?</h3>
       <BaseInput
         label="Location"
-        v-model=""
-        :error=""
+        v-model="location"
+        :error="errors.location"
         type="text"
       />
 
       <h3>Are pets allowed?</h3>
       <BaseRadioGroup
-        v-model=""
-        :error=""
+        v-model="pets"
+        :error="errors.pets"
         name="pets"
         :options="[
           { value: 1, label: 'Yes' },
@@ -47,16 +47,16 @@
       <div>
         <BaseCheckbox
           label="Catering"
-          v-model=""
-          :error=""
+          v-model="catering"
+          :error="errors.catering"
         />
       </div>
 
       <div>
         <BaseCheckbox
           label="Live music"
-          v-model=""
-          :error=""
+          v-model="music"
+          :error="errors.music"
         />
       </div>
 
@@ -74,7 +74,22 @@
 </template>
 
 <script>
+import { useField, useForm } from 'vee-validate'
+
 export default {
+  data () {
+    return {
+      categories: [
+        'sustainbility',
+        'natrue',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ]
+    }
+  },
   setup () {
     const required = value => {
       const requiredMessage = 'This field is required'
@@ -94,7 +109,56 @@ export default {
       return true
     }
 
-    return {}
+    const validationSchema = {
+      category: required,
+      title: value => {
+        const req = required(value)
+        if (req !== true) return req
+
+        const min = minLength(3, value)
+        if (min !== true) return min
+
+        return true
+      },
+      description: required,
+      location: undefined,
+      pets: anything,
+      catering: anything,
+      music: anything
+    }
+
+    const { handleSubmit, errors } = useForm({
+      validationSchema,
+      initialErrors: {
+        psts: 1,
+        catering: false,
+        music: false
+      }
+    })
+
+    const { value: category } = useField('category')
+    const { value: title } = useField('title')
+    const { value: description } = useField('description')
+    const { value: location } = useField('location')
+    const { value: pets } = useField('pets')
+    const { value: catering } = useField('catering')
+    const { value: music } = useField('music')
+
+    const submit = handleSubmit(values => {
+      console.log('submit', values)
+    })
+
+    return {
+      category,
+      title,
+      description,
+      location,
+      pets,
+      catering,
+      music,
+      submit,
+      errors
+    }
   }
 }
 </script>
